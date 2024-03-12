7150240b5761037ddc55151a9f67e7e3946c1654
alexw-su <alex.w.su@hotmail.com>
2024-03-12 14:01:58 +0100

    Health change

--- LD46/FlowerHealth.gd
+++ LD46/FlowerHealth.gd
@@ -5,19 +5,16 @@ func _process(delta):
 	if(gl.flowerHealth <= 3): 
 		for x in get_node("Flower4").get_children():
 			x.visible = false;
-	else: get_node("Flower4").set_process(true)
 	
 	if(gl.flowerHealth <= 2):
 		for x in get_node("Flower3").get_children():
 			x.visible = false;
-	else: get_node("Flower3").set_process(true)
 	
 	if(gl.flowerHealth <= 1):
 		for x in get_node("Flower2").get_children():
 			x.visible = false;
-	else: get_node("Flower2").set_process(true)
-	
+
 	if(gl.flowerHealth <= 0): 
 		for x in get_node("Flower").get_children():
 			x.visible = false;
-	else: get_node("Flower").set_process(true)
+
--- LD46/HEALTH.gd
+++ LD46/HEALTH.gd
@@ -1,5 +1,6 @@
-extends Label
+extends Node
 
 func _process(delta):
-	self.text = str(gl.health)
-	if(gl.health < 2): self.modulate = Color(255, 0, 0, 1)
+	if(gl.health <= 2): get_node("Heart3").visible = false
+	if(gl.health <= 1): get_node("Heart2").visible = false
+	if(gl.health <= 0): get_node("Heart").visible = false
