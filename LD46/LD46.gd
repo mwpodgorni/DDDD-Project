9bd599515cd28c1200018f6405cf035acc013eb4
mwpodgorni <mwpodgorni@gmail.com>
2024-04-15 23:12:46 +0200

    add game version

--- LD46/LD46.gd
+++ LD46/LD46.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var gameData=null;
 var currentGame = {
+		 "version": 'B',
 		 "timePassed":null,
 		 "timestamp": null,
 		 "droppedSwords": 0,
@@ -22,6 +23,7 @@ func _ready():
 
 func clearCurrentGame():
 	currentGame = {
+		 "version": 'B',
 		 "timestamp": null,
 		 "droppedSwords": 0,
 		 "dodgedSwords": 0,
